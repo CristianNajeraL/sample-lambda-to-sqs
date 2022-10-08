56c4f1d7be192e2eaea4260dbd02667187cdf7b5
Cristian Najera <45493332+CristianNajeraL@users.noreply.github.com>
2022-10-08 00:22:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,30 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-SECRETS_ARN = os.environ["SECRETS_ARN"]
-client = boto3.client('secretsmanager')
-secrets = eval(client.get_secret_value(SecretId=SECRETS_ARN)["SecretString"])
-user = secrets["db_username"]
-password = secrets["db_password"]
-host = secrets["db_host"]
-port = secrets["db_port"]
-db_name = secrets["db_database"]
-query = """SELECT usename AS role_name,
- CASE
-  WHEN usesuper AND usecreatedb THEN
-    CAST('superuser, create database' AS pg_catalog.text)
-  WHEN usesuper THEN
-    CAST('superuser' AS pg_catalog.text)
-  WHEN usecreatedb THEN
-    CAST('create database' AS pg_catalog.text)
-  ELSE
-    CAST('' AS pg_catalog.text)
- END role_attributes
-FROM pg_catalog.pg_user
-ORDER BY role_name desc;"""
-engine = create_engine(
-    f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
-)
+# SECRETS_ARN = os.environ["SECRETS_ARN"]
+# client = boto3.client('secretsmanager')
+# secrets = eval(client.get_secret_value(SecretId=SECRETS_ARN)["SecretString"])
+# user = secrets["db_username"]
+# password = secrets["db_password"]
+# host = secrets["db_host"]
+# port = secrets["db_port"]
+# db_name = secrets["db_database"]
+# query = """SELECT usename AS role_name,
+#  CASE
+#   WHEN usesuper AND usecreatedb THEN
+#     CAST('superuser, create database' AS pg_catalog.text)
+#   WHEN usesuper THEN
+#     CAST('superuser' AS pg_catalog.text)
+#   WHEN usecreatedb THEN
+#     CAST('create database' AS pg_catalog.text)
+#   ELSE
+#     CAST('' AS pg_catalog.text)
+#  END role_attributes
+# FROM pg_catalog.pg_user
+# ORDER BY role_name desc;"""
+# engine = create_engine(
+#     f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
+# )
 
 
 def lambda_handler(event, context):
@@ -39,13 +39,13 @@ def lambda_handler(event, context):
     Sample handler
     :return:
     """
-    df = pd.read_sql(
-        query,
-        con=engine
-    )
-    roles = list(df['role_name'].astype(str).values)
-    roles = ', '.join(roles)
+#     df = pd.read_sql(
+#         query,
+#         con=engine
+#     )
+#     roles = list(df['role_name'].astype(str).values)
+#     roles = ', '.join(roles)
     return {
-        "available_roles": roles,
+#         "available_roles": roles,
         "random": f"This is master = {random.random()}"
     }
